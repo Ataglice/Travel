82afb8f59df7c00d3c31fed772fff205130324aa
Attaglice <151725573+Ataglice@users.noreply.github.com>
2024-12-11 20:06:33 +0500

    основная версия

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,14 @@
+from setuptools.extern import names
+
 from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index),
-    path('about-us', views.about),
+    path('', views.index, name='home'),
+    path('about-us', views.about, name='about'),
+    path('trips', views.trips, name='trips'),
+    path('route', views.route, name='route'),
+    path('budget', views.budget, name='budget'),
+    path('tasks', views.tasks, name='tasks'),
+    path('cabinet', views.cabinet, name='cabinet'),
 ]
